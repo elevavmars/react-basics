f057a5afc0f469e188cabe0f598d92df7f428dfc
mars <marcus.metelis@outlook.com>
2024-10-27 21:05:22 +0000

    adicionar api

---
+++ aula-http/src/App.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import './style.css'
+
+// https://sujeitoprogramador.com/rn-api/?api=posts
+
+function App() {
+  const [nutri, setNutri] = useState([])
+
+  useEffect(() => {
+    function loadApi() {
+      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+
+      fetch(url)
+        .then((r) => r.json())
+        .then((json) => {
+          console.log(json);
+          setNutri(json);
+        })
+    }
+
+    loadApi();
+  }, []);
+
+  return (
+    <div className="container">
+      <header>
+        <strong>React Nutri</strong>
+      </header>
+    </div>
+  );
+}
+
+export default App;
