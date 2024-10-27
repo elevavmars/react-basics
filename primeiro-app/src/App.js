ed0da8380b12607110e9429035886d10144bdae5
mars <marcus.metelis@outlook.com>
2024-10-27 07:04:52 +0000

    atualizar useState para ser compativel com react

--- primeiro-app/src/App.js
+++ primeiro-app/src/App.js
@@ -9,10 +9,11 @@ function App() {
       'Estudar React JS'
     ];
   });
-  
+
+
   // Quando um componente é montado na tela o useEffect é chamado
   useEffect(() => {
-    const tarefasStorage = localStorage.getItem('tarefa');
+    const tarefasStorage = localStorage.getItem('@tarefa');
 
     if (tarefasStorage) {
       setTarefas(JSON.parse(tarefasStorage))
@@ -21,20 +22,20 @@ function App() {
   }, []); // Caso tenha algo nessa array então sempre q oq tiver na arry sofrer alteração o useEffect será chamado
 
   useEffect(() => {
-    localStorage.setItem('tarefa', JSON.stringify(tarefas))
+    localStorage.setItem('@tarefa', JSON.stringify(tarefas))
   }, [tarefas]); // setItem cria um item no locaStorage chamado @tarefa e transforma em string
-
 
   function handleRegister(e) {
     e.preventDefault();
 
     setTarefas([...tarefas, input]);
+    setInput('');
   }
 
   return (
 
     <div>
-      <h1>lista</h1>
+      <h1>Cadastrando usuário</h1>
 
       <form onSubmit={handleRegister}>
         <label>Nome da tarefa:</label><br />
